481982adbf13fc3482e175a987a5bbf8e45e3eff
Filip Peters <fpeters@vsdom.local>
2017-07-14 13:55:40 -0700

    initial wifi setup: if wifi isn't setup, or isn't available, then go into config mode. If the usr btn is pressed while powering on, force to go into config mode.

--- espLamp/espLamp.ino
+++ espLamp/espLamp.ino
@@ -1,16 +1,66 @@
 #include <ESP8266WiFi.h>
 
-#define ESP8266_LED 16
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>
 
-void setup() 
+#include <ArduinoJson.h>
+
+const int ESP8266_userBtn = 16;
+const int ESP8266_flashBtn = 0;
+
+void setup()
 {
-  pinMode(ESP8266_LED, OUTPUT);
+  //setup the two button pins
+  pinMode(ESP8266_userBtn, OUTPUT);
+  digitalWrite(ESP8266_userBtn, HIGH);
+  digitalWrite(ESP8266_flashBtn, HIGH);
+  pinMode(ESP8266_flashBtn, INPUT);
+
+  //setup a nice name before connecting, to find it in the list on the router
+  String hostName("ESPLamprey");
+  String hostID(ESP.getChipId(), HEX);
+  hostID.toUpperCase();
+  hostName += "-";
+  hostName += hostID;
+  WiFi.hostname(hostName);
+
+  //check if userbtn is pressed while starting, if so, go in config mode.
+  //otherwise try to do the autoconnect.
+  pinMode(ESP8266_userBtn, INPUT);
+  digitalWrite(ESP8266_userBtn, HIGH);
+  int btnVal = digitalRead(ESP8266_userBtn);
+  pinMode(ESP8266_userBtn, OUTPUT);
+
+  WiFiManager wifiManager;
+  wifiManager.setConfigPortalTimeout(180);
+  
+  if (btnVal == LOW)
+  {
+    //force config portal
+    digitalWrite(ESP8266_userBtn, LOW);
+    wifiManager.startConfigPortal (hostName.c_str());
+    digitalWrite(ESP8266_userBtn, HIGH);
+  }
+  else
+  {
+    wifiManager.autoConnect(hostName.c_str(), "mell");
+  }
+
+  //save settings (mqtt server) to a json file
+ 
 }
 
-void loop() 
+void loop()
 {
-  digitalWrite(ESP8266_LED, HIGH); // LED off
-  delay(500);
-  digitalWrite(ESP8266_LED, LOW); // LED on
-  delay(500);
+
+
+  if (digitalRead(ESP8266_flashBtn) == HIGH)
+  {
+    digitalWrite(ESP8266_userBtn, HIGH);
+  }
+  else
+  {
+    digitalWrite(ESP8266_userBtn, LOW);
+  }
 }
