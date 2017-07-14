5b9d05de13435626aed674e77753a54c14a88994
Filip Peters <fpeters@vsdom.local>
2017-07-14 14:58:44 -0700

    trying out astyle in microsoft code

--- espLamp/espLamp.ino
+++ espLamp/espLamp.ino
@@ -11,41 +11,41 @@ const int ESP8266_flashBtn = 0;
 
 void setup()
 {
-	//setup the two button pins
-	pinMode (ESP8266_userBtn, OUTPUT);
-	digitalWrite (ESP8266_userBtn, HIGH);
-	digitalWrite (ESP8266_flashBtn, HIGH);
-	pinMode (ESP8266_flashBtn, INPUT);
+    //setup the two button pins
+    pinMode (ESP8266_userBtn, OUTPUT);
+    digitalWrite (ESP8266_userBtn, HIGH);
+    digitalWrite (ESP8266_flashBtn, HIGH);
+    pinMode (ESP8266_flashBtn, INPUT);
 
-	//setup a nice name before connecting, to find it in the list on the router
-	String hostName ("ESPLamprey");
-	String hostID (ESP.getChipId(), HEX);
-	hostID.toUpperCase();
-	hostName += "-";
-	hostName += hostID;
-	WiFi.hostname (hostName);
+    //setup a nice name before connecting, to find it in the list on the router
+    String hostName ("ESPLamprey");
+    String hostID (ESP.getChipId(), HEX);
+    hostID.toUpperCase();
+    hostName += "-";
+    hostName += hostID;
+    WiFi.hostname (hostName);
 
-	//check if userbtn is pressed while starting, if so, go in config mode.
-	//otherwise try to do the autoconnect.
-	pinMode (ESP8266_userBtn, INPUT);
-	digitalWrite (ESP8266_userBtn, HIGH);
-	int btnVal = digitalRead (ESP8266_userBtn);
-	pinMode (ESP8266_userBtn, OUTPUT);
+    //check if userbtn is pressed while starting, if so, go in config mode.
+    //otherwise try to do the autoconnect.
+    pinMode (ESP8266_userBtn, INPUT);
+    digitalWrite (ESP8266_userBtn, HIGH);
+    int btnVal = digitalRead (ESP8266_userBtn);
+    pinMode (ESP8266_userBtn, OUTPUT);
 
-	WiFiManager wifiManager;
-	wifiManager.setConfigPortalTimeout (180);
+    WiFiManager wifiManager;
+    wifiManager.setConfigPortalTimeout (180);
 
-	if (btnVal == LOW)
-	{
-		//force config portal
-		digitalWrite (ESP8266_userBtn, LOW);
-		wifiManager.startConfigPortal (hostName.c_str());
-		digitalWrite (ESP8266_userBtn, HIGH);
-	}
-	else
-		wifiManager.autoConnect (hostName.c_str());
+    if (btnVal == LOW)
+    {
+        //force config portal
+        digitalWrite (ESP8266_userBtn, LOW);
+        wifiManager.startConfigPortal (hostName.c_str());
+        digitalWrite (ESP8266_userBtn, HIGH);
+    }
+    else
+        wifiManager.autoConnect (hostName.c_str());
 
-	//save settings (mqtt server) to a json file
+    //save settings (mqtt server) to a json file
 
 }
 
@@ -53,8 +53,8 @@ void loop()
 {
 
 
-	if (digitalRead (ESP8266_flashBtn) == HIGH)
-		digitalWrite (ESP8266_userBtn, HIGH);
-	else
-		digitalWrite (ESP8266_userBtn, LOW);
+    if (digitalRead (ESP8266_flashBtn) == HIGH)
+        digitalWrite (ESP8266_userBtn, HIGH);
+    else
+        digitalWrite (ESP8266_userBtn, LOW);
 }
