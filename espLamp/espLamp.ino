f821a5f49717d5f6548b102bc455541bad99aca9
Filip Peters <fpeters@vsdom.local>
2017-07-17 13:03:25 -0700

    added code from sample to read/write config settings for the mqtt server

--- espLamp/espLamp.ino
+++ espLamp/espLamp.ino
@@ -1,14 +1,40 @@
+#include "FS.h"
 #include <ESP8266WiFi.h>
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>
+#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
 
-#include <ArduinoJson.h>
+#include <PubSubClient.h>         // https://github.com/knolleary/pubsubclient
+#include <ArduinoJson.h>          // https://github.com/bblanchon/ArduinoJson
+
+#if defined(DEBUG_TELNET)
+WiFiServer  telnetServer (23);
+WiFiClient  telnetClient;
+#define     DEBUG_PRINT(x)    telnetClient.print (x)
+#define     DEBUG_PRINTLN(x)  telnetClient.println (x)
+#elif defined(DEBUG_SERIAL)
+#define     DEBUG_PRINT(x)    Serial.print (x)
+#define     DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+#define     DEBUG_PRINT(x) ;
+#define     DEBUG_PRINTLN(x) ;
+#endif
 
 const int ESP8266_userBtn = 16;
 const int ESP8266_flashBtn = 0;
 
+//define your default values here, if there are different values in config.json, they are overwritten.
+char mqttServer[40];
+char mqttPort[6] = "8080";
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback ()
+{
+    DEBUG_PRINTLN ("Should save config");
+    shouldSaveConfig = true;
+}
 void setup()
 {
     //setup the two button pins
@@ -16,6 +42,17 @@ void setup()
     digitalWrite (ESP8266_userBtn, HIGH);
     digitalWrite (ESP8266_flashBtn, HIGH);
     pinMode (ESP8266_flashBtn, INPUT);
+
+    //check if userbtn is pressed while starting, if so, go in config mode.
+    //otherwise try to do the autoconnect.
+    pinMode (ESP8266_userBtn, INPUT);
+    digitalWrite (ESP8266_userBtn, HIGH);
+    int btnVal = digitalRead (ESP8266_userBtn);
+    pinMode (ESP8266_userBtn, OUTPUT);
+
+    //turn led on to acknowledge the btn was pressed on power up
+    if (btnVal == LOW)
+        digitalWrite (ESP8266_userBtn, LOW);
 
     //setup a nice name before connecting, to find it in the list on the router
     String hostName ("ESPLamprey");
@@ -25,15 +62,56 @@ void setup()
     hostName += hostID;
     WiFi.hostname (hostName);
 
-    //check if userbtn is pressed while starting, if so, go in config mode.
-    //otherwise try to do the autoconnect.
-    pinMode (ESP8266_userBtn, INPUT);
-    digitalWrite (ESP8266_userBtn, HIGH);
-    int btnVal = digitalRead (ESP8266_userBtn);
-    pinMode (ESP8266_userBtn, OUTPUT);
+    //load the file system, read some settings for the mqtt server and last used display settings
+    if (SPIFFS.begin())
+    {
+        DEBUG_PRINTLN ("mounted FS");
+        if (SPIFFS.exists ("/config.json"))
+        {
+            DEBUG_PRINTLN ("loaded json config");
+            File configFile = SPIFFS.open ("/config.json", "r");
+            if (configFile)
+            {
+                DEBUG_PRINTLN ("opened config file");
+                size_t size = configFile.size();
+
+                // Allocate a buffer to store contents of the file.
+                std::unique_ptr<char[]> buf (new char[size]);
+
+                configFile.readBytes (buf.get(), size);
+                DynamicJsonBuffer jsonBuffer;
+                JsonObject& json = jsonBuffer.parseObject (buf.get());
+                json.printTo (Serial);
+                if (json.success())
+                {
+                    DEBUG_PRINTLN ("parsed json");
+
+                    strcpy (mqttServer, json["mqtt_server"]);
+                    strcpy (mqttPort, json["mqtt_port"]);
+                }
+                else
+                    DEBUG_PRINTLN ("failed to load json config");
+            }
+        }
+        else
+            DEBUG_PRINTLN ("failed to load json config");
+    }
+    else
+        DEBUG_PRINTLN ("failed to mount FS");
 
     WiFiManager wifiManager;
+
+    //set a timeout value. if it can't connect, then at least revert to last used light state
     wifiManager.setConfigPortalTimeout (180);
+
+    //set config save notify callback
+    wifiManager.setSaveConfigCallback (saveConfigCallback);
+
+    //add custom params to wifimanager
+    WiFiManagerParameter customMqttServer ("server", "mqtt server", mqttServer, 40);
+    WiFiManagerParameter customMqttPort ("port", "mqtt port", mqttPort, 6);
+    wifiManager.addParameter (&customMqttServer);
+    wifiManager.addParameter (&customMqttPort);
 
     if (btnVal == LOW)
     {
@@ -46,8 +124,27 @@ void setup()
         wifiManager.autoConnect (hostName.c_str());
 
     //save current settings (mqtt server) to the json settings file
+    strcpy (mqttServer, customMqttServer.getValue());
+    strcpy (mqttPort, customMqttPort.getValue());
+    if (shouldSaveConfig)
+    {
+        DEBUG_PRINTLN ("saving config");
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.createObject();
+        json["mqtt_server"] = mqttServer;
+        json["mqtt_port"] = mqttPort;
 
-    //subscribe, start connection with the mqtt server
+        File configFile = SPIFFS.open ("/config.json", "w");
+        if (!configFile)
+            DEBUG_PRINTLN ("failed to open config file for writing");
+        else
+        {
+            json.printTo (configFile);
+            configFile.close();
+        }
+    }
+
+    //subscribe, start connection with the mqtt server (if address and port are set)
 
 }
 
